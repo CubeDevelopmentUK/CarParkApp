34e47cf53fcf9c015a2899de3296a04ce9b1545f
Cube Development <barber.j@cubedevelopment.co.uk>
2015-12-02 16:14:57 +0000

    New commit

--- CarParkApp/Form1.Designer.cs
+++ CarParkApp/Form1.Designer.cs
@@ -32,16 +32,19 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lblStandardSpaces = new System.Windows.Forms.Label();
             this.lblDisabledSpaces = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblStandardTitle = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.addEditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editNumberOfSpacesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.addNewCarParkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.standardSpacesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.disabledSpacesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewCarParkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -61,7 +64,7 @@
             // 
             this.groupBox1.Controls.Add(this.lblStandardSpaces);
             this.groupBox1.Controls.Add(this.lblDisabledSpaces);
-            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblStandardTitle);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12F);
             this.groupBox1.Location = new System.Drawing.Point(6, 73);
@@ -80,7 +83,7 @@
             this.lblStandardSpaces.Name = "lblStandardSpaces";
             this.lblStandardSpaces.Size = new System.Drawing.Size(19, 19);
             this.lblStandardSpaces.TabIndex = 3;
-            this.lblStandardSpaces.Text = "0";
+            this.lblStandardSpaces.Text = "5";
             // 
             // lblDisabledSpaces
             // 
@@ -92,15 +95,15 @@
             this.lblDisabledSpaces.TabIndex = 1;
             this.lblDisabledSpaces.Text = "0";
             // 
-            // label2
+            // lblStandardTitle
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 44);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 19);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Standard:";
+            this.lblStandardTitle.AutoSize = true;
+            this.lblStandardTitle.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStandardTitle.Location = new System.Drawing.Point(3, 44);
+            this.lblStandardTitle.Name = "lblStandardTitle";
+            this.lblStandardTitle.Size = new System.Drawing.Size(78, 19);
+            this.lblStandardTitle.TabIndex = 2;
+            this.lblStandardTitle.Text = "Standard:";
             // 
             // label1
             // 
@@ -118,7 +121,7 @@
             this.addEditToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1276, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(765, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -133,8 +136,9 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
             this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // addEditToolStripMenuItem
             // 
@@ -154,12 +158,6 @@
             this.editNumberOfSpacesToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
             this.editNumberOfSpacesToolStripMenuItem.Text = "Edit Number of Spaces";
             // 
-            // addNewCarParkToolStripMenuItem
-            // 
-            this.addNewCarParkToolStripMenuItem.Name = "addNewCarParkToolStripMenuItem";
-            this.addNewCarParkToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
-            this.addNewCarParkToolStripMenuItem.Text = "Add New Car Park";
-            // 
             // standardSpacesToolStripMenuItem
             // 
             this.standardSpacesToolStripMenuItem.Name = "standardSpacesToolStripMenuItem";
@@ -172,14 +170,54 @@
             this.disabledSpacesToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
             this.disabledSpacesToolStripMenuItem.Text = "Disabled Spaces";
             // 
+            // addNewCarParkToolStripMenuItem
+            // 
+            this.addNewCarParkToolStripMenuItem.Name = "addNewCarParkToolStripMenuItem";
+            this.addNewCarParkToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.addNewCarParkToolStripMenuItem.Text = "Add New Car Park";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.btnEnter.Location = new System.Drawing.Point(6, 159);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(147, 57);
+            this.btnEnter.TabIndex = 3;
+            this.btnEnter.Text = "Enter Car Park";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(502, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 137);
+            this.listBox1.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(492, 33);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(273, 180);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Coins";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(40F, 77F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1276, 773);
+            this.ClientSize = new System.Drawing.Size(765, 292);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnEnter);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox2);
             this.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(20, 18, 20, 18);
@@ -200,7 +238,7 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label lblStandardSpaces;
         private System.Windows.Forms.Label lblDisabledSpaces;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStandardTitle;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem addEditToolStripMenuItem;
@@ -210,6 +248,9 @@
         private System.Windows.Forms.ToolStripMenuItem addNewCarParkToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- CarParkApp/Form1.cs
+++ CarParkApp/Form1.cs
@@ -12,19 +12,48 @@ namespace CarParkApp
 {
     public partial class Form1 : Form
     {
+        string[] coins = new string[4];
+        int valuesEntered;
+        int spaces = 4;
+
         public Form1()
         {
             InitializeComponent();
+            valuesEntered = 0;
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
-            int spaces = 100;
-            if (spaces == 100)
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            if (valuesEntered == 4)
             {
+                MessageBox.Show("No spaces available");
+                btnEnter.Enabled = false;
+                lblStandardSpaces.ForeColor = System.Drawing.Color.Red;
+                lblStandardTitle.ForeColor = System.Drawing.Color.Red;
                 groupBox1.ForeColor = System.Drawing.Color.Red;
             }
+            else
+            {
+                coins[valuesEntered] = "coin" + valuesEntered.ToString();
+                valuesEntered++;
+                listBox1.Items.Add(coins);
+                spaces--;
+            }
+            lblStandardSpaces.Text = spaces.ToString();
+        }
+        private void lblCoinDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
